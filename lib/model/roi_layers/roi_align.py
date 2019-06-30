27bd7de2c0b51a4cbfc0f0db9fe8060b733d8983
Abhimanyu Talwar <p10abhimanyut@iima.ac.in>
2019-06-30 11:34:25 +0800

    replaced import from model with lib.model in roi_align.py

--- lib/model/roi_layers/roi_align.py
+++ lib/model/roi_layers/roi_align.py
@@ -5,7 +5,7 @@ from torch.autograd import Function
 from torch.autograd.function import once_differentiable
 from torch.nn.modules.utils import _pair
 
-from model import _C
+from lib.model import _C
 
 import pdb
 
